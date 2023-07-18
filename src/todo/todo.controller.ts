9c392fcece5c65e29c39deacdd51f229895d5da6
hoang.nguyennhat(VTI.D8) <hoang.nguyennhat@vti.com.vn>
2023-07-18 10:29:41 +0700

    ;';;;

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,18 +1,48 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller, Delete,
+  Get,
+  Logger,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put
+} from "@nestjs/common";
 import { TodoService } from './todo.service';
 import { Todo } from './todo.interface';
 
 @Controller('todo')
 export class TodoController {
+  private readonly logger = new Logger(TodoController.name);
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
   findAll(): Todo[] {
+    this.logger.log('Handling findAll() request...');
     return this.todoService.fillAll();
   }
 
   @Post()
   createTodo(@Body() todo: Todo): void {
+    this.logger.log('handle create() request', todo);
     return this.todoService.create(todo);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Todo {
+    this.logger.log('Handling findOne() request with id=' + id + '...');
+    return this.todoService.findOne(id);
+  }
+
+  @Put(':id')
+  updateTodo(@Param('id', ParseIntPipe) id: number, @Body() todo: Todo): Todo {
+    this.logger.log('Handling findOne() request with id = ' + id + '....');
+    return this.todoService.update(id, todo);
+  }
+
+  @Delete(':id')
+  removeTodo(@Param('íd', ParseIntPipe) id: number) {
+    this.logger.log('Handing delete request with id' + id + '....');
+    return this.todoService.remove(id);
   }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,15 +3,37 @@ import { Todo } from './todo.interface';
 
 @Injectable()
 export class TodoService {
-  private storage: Todo[] = [];
+  private storage: Todo[] = [
+    {
+      label: 'Create an awesome API',
+      complete: true,
+      id: 1,
+    },
+  ];
 
   create(todo: Todo): void {
-    const curentMaxId = Math.max(...this.storage.map((t: Todo) => t.id));
-    todo.id = curentMaxId + 1;
+    const currentMaxId = Math.max(...this.storage.map((t: Todo) => t.id));
+    todo.id = currentMaxId + 1;
     this.storage.push(todo);
   }
 
   fillAll(): Todo[] {
+    return this.storage;
+  }
+
+  findOne(id: number): Todo {
+    return this.storage.find((t: Todo) => t.id === id);
+  }
+
+  update(id: number, todo: Todo): Todo {
+    const index = this.storage.findIndex((todo: Todo) => todo.id === id);
+    this.storage[index] = todo;
+    return this.storage[index];
+  }
+
+  remove(id) {
+    const index = this.storage.findIndex((t: Todo) => t.id === id);
+    this.storage.splice(index, 1);
     return this.storage;
   }
 }
